990a537664bdb4efba2c9b767659f991fec8fcc6
Amaraa0505 <amarsanaa.tumurbaatar26@gmail.com>
2023-10-13 14:44:06 +0700

    zaslaa

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -3,15 +3,15 @@ function Work() {
     {
       name: "MARS-V",
       passage:
-        "when i was 16 years old i firstly enrolled like volunteer this project that build camp to prepare astronauts who is going to mars and i sometimes used to work. After december in 2023, i recieved suggest to work at mars-v project as a full-time in team of module development",
+        "3 years ago when i used to work like volunteer this project that build camp to prepare astronauts and .in 2023, i recieved suggest to work at as a full-time in team of module development",
     },
-    
+
     {
       name: "SPACE WEEK",
       passage:
         "last year i worked for a week as a volunteer and then i leared so many ability by communicating people. It was so memorable and useful to me",
     },
-    
+
     {
       name: "HOT FESTIVAL",
       passage:
@@ -24,23 +24,23 @@ function Work() {
     },
   ];
   return (
-    <div className="grid grid-cols-4 h-full">
-      <div className="flex-1  flex items-center justify-center">
+    <div className="grid grid-cols-4 h-full bg-[#f0f9ff]">
+      <div className="col-span-1">
         <img
-          className="cols-span-1"
+          className="h-full"
           src="https://images.unsplash.com/photo-1541873676-a18131494184?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXN0cm9uYXV0fGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60"
         />
       </div>
-      <div className="col-span-3 items-center pl-6 bg-[#f0f9ff]">
-      <div className="flex gap-8">
-       {works.map((el) => (
-          <div className=" rounded-2xl p-8 ">
-            <h3 className="font-bold">{el.name}</h3>
-            <div className="h-0.5 bg-gray-600 rounded-xl"></div>
-            <p className="w-64">{el.passage}</p>
-          </div>
-        ))}
-      </div>
+      <div className="col-span-3 items-center">
+        <div className="grid grid-cols-2">
+          {works.map((el) => (
+            <div className="rounded-2xl grid gap-8 p-8 bg-[#f0f9ff]">
+              <h3 className="font-bold">{el.name}</h3>
+              <div className="h-0.5 bg-gray-600 rounded-xl"></div>
+              <p className="w-64">{el.passage}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
